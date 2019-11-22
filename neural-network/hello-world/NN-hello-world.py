6379e99e1e407aaf503a33de413b84610efd8792
M. Imam Pratama <imambungo@gmail.com>
2019-11-22 17:03:35 +0700

    Hello World nya NN

--- neural-network/hello-world/NN-hello-world.py
+++ neural-network/hello-world/NN-hello-world.py
@@ -1,6 +1,6 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
-clf = DecisionTreeClassifier()
+clf = MLPClassifier()
 clf = clf.fit(features, labels)
 print(clf.predict([[150, 0]]))
