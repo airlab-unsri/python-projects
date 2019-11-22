8a5ddad3def45876d6ffcf6bf3003717b56066ec
M. Imam Pratama <imambungo@gmail.com>
2019-11-22 17:01:12 +0700

    hello world of machine learning
    machine learning in 6 lines of code

---
+++ neural-network/hello-world/NN-hello-world.py
@@ -0,0 +1,6 @@
+from sklearn.tree import DecisionTreeClassifier
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[150, 0]]))
